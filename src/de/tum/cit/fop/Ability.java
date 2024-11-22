36ec5ee920d7f627c58316b2d0fc69a6db49fc06
ebrahimudaipurwala <ebrahim.udaipurwala@gmail.com>
2024-11-22 10:57:25 +0100

    Part 1 Task 1

--- src/de/tum/cit/fop/Ability.java
+++ src/de/tum/cit/fop/Ability.java
@@ -8,12 +8,33 @@ public class Ability {
     private int heal;
     private int armor;
 
-    public Ability(String name, String specification, int damage, int heal, int armor) {
+    public Ability(String name, String specification, int amount) {
         this.name = name;
         this.specification = specification;
-        this.damage = damage;
-        this.heal = heal;
-        this.armor = armor;
+        if (specification.equals("Mage")){
+            this.specification = "Mage";
+            this.damage = amount;
+            this.heal = -1;
+            this.armor = -1;
+        }
+        else if (specification.equals("Tank")){
+            this.specification = "Tank";
+            this.damage = -1;
+            this.heal = -1;
+            this.armor = amount;
+        }
+        else if (specification.equals("Healer")){
+            this.specification = "Healer";
+            this.damage = -1;
+            this.heal = amount;
+            this.armor = -1;
+        }
+        else if (specification.equals("Warrior")){
+            this.specification = "Warrior";
+            this.damage = amount;
+            this.heal = -1;
+            this.armor = -1;
+        }
     }
 
     public String getName() {
--- src/de/tum/cit/fop/Item.java
+++ src/de/tum/cit/fop/Item.java
@@ -1,5 +1,79 @@
 package de.tum.cit.fop;
 
 public abstract class Item {
-  // TODO: Implement part 2.
+    protected String specification;
+    protected String type;
+    protected boolean equipped;
+    protected int strength;
+    protected int intelligence;
+    protected int agility;
+    protected int spirit;
+
+    public Item(String specification, String type, boolean equipped, int strength, int intelligence, int agility, int spirit) {
+        this.specification = specification;
+        this.type = type;
+        this.equipped = equipped;
+        this.strength = strength;
+        this.intelligence = intelligence;
+        this.agility = agility;
+        this.spirit = spirit;
+    }
+
+    public String getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(String specification) {
+        this.specification = specification;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isEquipped() {
+        return equipped;
+    }
+
+    public void setEquipped(boolean equipped) {
+        this.equipped = equipped;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public int getSpirit() {
+        return spirit;
+    }
+
+    public void setSpirit(int spirit) {
+        this.spirit = spirit;
+    }
+
+    // TODO: Implement part 2.
 }
