ec8ca0d8c65a8cebd4cce21fdf394c26a597e635
ebrahimudaipurwala <ebrahim.udaipurwala@gmail.com>
2024-11-22 18:06:00 +0100

    Part 3

--- src/de/tum/cit/fop/Player.java
+++ src/de/tum/cit/fop/Player.java
@@ -1,9 +1,8 @@
 package de.tum.cit.fop;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Player {
+public abstract class Player implements Interactions {
     protected String name;
     protected String specification;
     protected int amountOfArmor;
@@ -29,7 +28,56 @@ public abstract class Player {
         this.armor = armor;
         this.weapon = weapon;
         this.weaponType = weaponType;
+        equipItems();
     }
+
+    public void equipItems(){
+        for (Armor armor : armor) {
+            if(armor.getType().equalsIgnoreCase("Helmet") && helmet == null){
+                helmet = armor;
+                updateStats(armor);
+            }
+            else if (armor.getType().equalsIgnoreCase("Chest") && chest == null){
+                chest = armor;
+                updateStats(armor);
+            }
+            else if (armor.getType().equalsIgnoreCase("Legs") && legs == null){
+                legs = armor;
+                updateStats(armor);
+            }
+            else if (armor.getType().equalsIgnoreCase("Boots") && boots == null){
+                boots = armor;
+                updateStats(armor);
+            }
+        }
+        if(weapon != null && weapon.getType().equalsIgnoreCase(weaponType)){
+            updateStats(weapon);
+        }
+    }
+
+    private void updateStats(Item item){
+        this.strength += item.getStrength();
+        this.intelligence += item.getIntelligence();
+        this.agility += item.getAgility();
+        this.spirit += item.getSpirit();
+
+        if (item instanceof Armor) {
+            this.amountOfArmor += ((Armor) item).getAmountOfArmor();
+        }
+        else if (item instanceof Weapon){
+            this.strength += ((Weapon)item).getDamage();
+        }
+    }
+
+    protected boolean isDead(){
+        return health <= 0;
+    }
+
+    @Override
+    public abstract void attack(Player target);
+
+    @Override
+    public abstract void useAbility(Player target);
 
     // TODO: Add missing attributes.
 
