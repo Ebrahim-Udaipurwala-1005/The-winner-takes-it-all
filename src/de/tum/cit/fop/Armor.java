eff1a7e1fc8d88dc7304bc10637205bca90f449c
ebrahimudaipurwala <ebrahim.udaipurwala@gmail.com>
2024-11-22 11:18:39 +0100

    Part 1 Task 1

--- src/de/tum/cit/fop/Armor.java
+++ src/de/tum/cit/fop/Armor.java
@@ -1,8 +1,5 @@
 package de.tum.cit.fop;
 
-public class Armor extends Item{
+public class Armor{
     private int amountOfArmor;
-    public Armor(String armorType, String specification, int amountOfArmor, int strength, int intelligence, int agility, int spirit){
-
-    }
 }
