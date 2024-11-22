7abd6b860d1cb6e23fbabe30b6c316bd54b72fd7
ebrahimudaipurwala <ebrahim.udaipurwala@gmail.com>
2024-11-22 11:27:02 +0100

    Part 1 Task 2

--- src/de/tum/cit/fop/Armor.java
+++ src/de/tum/cit/fop/Armor.java
@@ -1,5 +1,14 @@
 package de.tum.cit.fop;
 
-public class Armor{
+public class Armor extends Item {
     private int amountOfArmor;
+    public Armor(String armorType, String specification, int amountOfArmor, int strength, int intelligence, int agility, int spirit) {
+        super(armorType);
+        this.specification = specification;
+        this.amountOfArmor = amountOfArmor;
+        this.strength = strength;
+        this.intelligence = intelligence;
+        this.agility = agility;
+        this.spirit = spirit;
+    }
 }
--- src/de/tum/cit/fop/Weapon.java
+++ src/de/tum/cit/fop/Weapon.java
@@ -1,5 +1,6 @@
 package de.tum.cit.fop;
 
 public class Weapon {
+    private int damage;
   // TODO: Implement part 2.
 }
