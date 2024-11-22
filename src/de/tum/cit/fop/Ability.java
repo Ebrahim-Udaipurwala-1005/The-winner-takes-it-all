171d569e7ae61da2d4bec5f994f16ea9d17f21f3
ebrahimudaipurwala <ebrahim.udaipurwala@gmail.com>
2024-11-22 10:32:16 +0100

    Part 1 Task 1

--- src/de/tum/cit/fop/Ability.java
+++ src/de/tum/cit/fop/Ability.java
@@ -8,7 +8,13 @@ public class Ability {
     private int heal;
     private int armor;
 
-    // TODO: Constructor
+    public Ability(String name, String specification, int damage, int heal, int armor) {
+        this.name = name;
+        this.specification = specification;
+        this.damage = damage;
+        this.heal = heal;
+        this.armor = armor;
+    }
 
     public String getName() {
         return name;
