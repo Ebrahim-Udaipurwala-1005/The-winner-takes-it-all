4b95418a017014d1670f973a662a48b707d46bdc
ebrahimudaipurwala <ebrahim.udaipurwala@gmail.com>
2024-11-22 11:39:25 +0100

    Part 3

--- src/de/tum/cit/fop/Player.java
+++ src/de/tum/cit/fop/Player.java
@@ -1,6 +1,21 @@
 package de.tum.cit.fop;
 
 public abstract class Player {
+    protected String name;
+    protected String specification;
+    protected int amountOfArmor;
+    protected String weaponType;
+    protected int strength;
+    protected int intelligence;
+    protected int agility;
+    protected int spirit;
+    protected double health;
+    protected int level;
+    protected Armor helmet;
+    protected Armor chest;
+    protected Armor hands;
+    protected Armor legs;
+    protected Armor boots;
   // TODO: Add missing attributes.
 
   // TODO: Implement part 3.
